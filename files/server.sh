15333f424753b5c9e39fcf5bd198f3aa1c763568
Cliff Rowley <cliffrowley@gmail.com>
2017-07-03 21:04:59 +0100

    Typo

--- files/server.sh
+++ files/server.sh
@@ -3,7 +3,7 @@
 export LD_LIBRARY_PATH=$(dirname $SDTD_DIR)
 
 if [ -n "STEAMCMD_APP_BETA" ]; then
-  BETA="-beta $STEAM_APP_BETA"
+  BETA="-beta $STEAMCMD_APP_BETA"
 fi
 
 steamcmd.sh +login anonymous +force_install_dir $SDTD_DIR +app_update $STEAM_APP_ID $BETA +quit
